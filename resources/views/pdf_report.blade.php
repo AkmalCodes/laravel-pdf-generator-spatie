a5204fd7614b4276ddc045def8d75e5ffb75b3df
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-09-21 09:39:38 +0800

    update -- added new cell to hold data for item distribution by weight

--- resources/views/pdf_report.blade.php
+++ resources/views/pdf_report.blade.php
@@ -3,8 +3,6 @@
 
 <head>
     <title>Inventory Report</title>
-    <!-- Bootstrap CSS v5.2.1 -->
-
     <style>
         @page {
             margin: 5 5 5 5; /* top, right, bottom, left */
@@ -72,10 +70,10 @@
 <body>
     <div class="header">
         <div class="container d-flex flex-column align-items-center">
-            <img class="img-logo" src="{{$m9_logo }}" alt="Merchant9 Logo">
+            <img class="img-logo" src="{{ $m9_logo }}" alt="Merchant9 Logo">
             <div class="header-details">
-                <h1 style="position: relative; left:-5px;">Sales Report</h1>
-                <h4 style="position: relative; left:5px;">Sale period: <strong>{{ $report_generated_at_start }}</strong> to
+                <h1>Sales Report</h1>
+                <h4>Sale period: <strong>{{ $report_generated_at_start }}</strong> to
                     <strong>{{ $report_generated_at_end }}</strong>
                 </h4>
             </div>
@@ -87,8 +85,9 @@
                 <tr>
                     <th>Description</th>
                     <th>Category Code</th>
-                    <th>Items Sold</th>
+                    <th>Gold Weight Categories</th>
                     <th>Total Sales Amount</th>
+                    <th>Items Sold</th>
                 </tr>
             </thead>
             @foreach ($inventoryData as $description => $data)
@@ -97,44 +96,45 @@
                         <td>{{ $description }}</td>
                         <td>{{ $data['category_code'] }}</td>
                         <td>
+                            @php
+                            // Check if all gold weight categories are empty or zero
+                            $allEmpty = true;
+                            $categories = ['0-5', '5-10', '10-15', '15+', '20+'];
+                            
+                            // Check if any category has a non-zero value
+                            foreach ($categories as $category) {
+                                if (!empty($data['gold_weight_categories'][$category])) {
+                                    $allEmpty = false;
+                                    break;
+                                }
+                            }
+                            @endphp
+                            @if($allEmpty)
+                                <strong>Items calculated by individual product instead of gold weight.</strong>
+                            @else
+                                <div>0-5g: {{ $data['gold_weight_categories']['0-5'] ?? 0 }}</div>
+                                <div>5-10g: {{ $data['gold_weight_categories']['5-10'] ?? 0 }}</div>
+                                <div>10-15g: {{ $data['gold_weight_categories']['10-15'] ?? 0 }}</div>
+                                <div>15+g: {{ $data['gold_weight_categories']['15+'] ?? 0 }}</div>
+                                <div>20+g: {{ $data['gold_weight_categories']['20+'] ?? 0 }}</div>
+                            @endif
+                        </td>                        
+                        <td>{{ $data['sales_amount'] }}</td>
+                        <td>
                             @foreach ($data['store_counts'] as $storecode => $count)
-                            <div class="container p-0">
-                                {{ $storecode }}: {{ $count }}
-                            </div>
+                                <div class="container p-0">
+                                    <strong>{{ $storecode }}:</strong>  {{ $count }}
+                                </div>
                             @endforeach
                             <div class="container p-0">
-                                total: {{ $data['items_sold_count']}}
+                                <strong>Total:</strong> {{ $data['items_sold_count'] }}
                             </div>
                         </td>
-                        <td>{{ $data['sales_amount'] }}</td>
                     </tr>
-                    {{-- <tr>
-                    <th colspan="5" class="text-center">Items Sold Per Store</th>
-                </tr>
-                <tr>
-                    <th>Item No.</th>
-                    <th>Inventory Code</th>
-                    <th colspan="3">Store Code</th>
-                </tr> --}}
                 </tbody>
-                {{-- <tbody>
-                    @foreach ($data['inventory_codes'] as $index => $code)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $code }}</td>
-                        </tr>
-                    @endforeach
-                </tbody> --}}
             @endforeach
         </table>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
